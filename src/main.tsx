0fe818200f03994f5782bc44c3a5222011222581
Mkhisamo <mkhisamo@MacBook-Air-Mkhisamo.local>
2026-01-12 11:47:27 +0300

    Desktop shows mobile layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,10 @@
   import App from "./App.tsx";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+  createRoot(document.getElementById("root")!).render(
+  <div className="page-bg">
+    <div className="mobile-shell">
+      <App />
+    </div>
+  </div>
+);
